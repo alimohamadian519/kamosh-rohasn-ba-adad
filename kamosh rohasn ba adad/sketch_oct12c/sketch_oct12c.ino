c8b07d9d3fdb97644c5cf1bb40c6e83170962062
alimohamadian519 <mohamadianali92@gmail.com>
2024-10-18 20:29:17 +0330

    Add files via upload
    add file sketch_oct12c

---
+++ kamosh rohasn ba adad/sketch_oct12c/sketch_oct12c.ino
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#include <math.h>
+
+void setup() { Serial.begin(9600);
+}
+void loop() { 
+  for (float = 0; i<360.0+2)
+  ( Serial.print(sin(1*PI/100.00)); 
+  Serial.print(" ");
+   Serial.println(cos(i*PI/100.00));
+
+}
+
+}
